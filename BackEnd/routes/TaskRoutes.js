017fc4bf18917668b4d18b81c671652568bc64c3
MarcosMartinsDeLima <0timetraveler@gmail.com>
2024-10-10 17:00:43 -0300

    feat: rota de task

--- BackEnd/routes/TaskRoutes.js
+++ BackEnd/routes/TaskRoutes.js
@@ -1,1 +1,12 @@
 const router = require('express').Router()
+const TaskController = require("../controllers/TaskController")
+const verifyToken = require("../helper/verifyToken")
+
+router.post("/create",verifyToken,TaskController.createTask)
+router.get("/all",verifyToken,TaskController.getAllTasks)
+router.get("/:id",verifyToken,TaskController.getById)
+router.patch("/updateStatusById/:id",verifyToken,TaskController.updateStatus)
+router.put("/updateById/:id",verifyToken,TaskController.updateTarefa)
+router.delete("/deleteById/:id",verifyToken,TaskController.delete)
+
+module.exports = router
