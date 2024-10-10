26986e8e5991d15b8f33ca4277e3baaa42e0137f
MarcosMartinsDeLima <0timetraveler@gmail.com>
2024-10-10 13:52:21 -0300

    feat: adicionando rota user

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const connection = require("./db/connection")
 
+const UserRoutes = require("./routes/UserRoutes")
+
 const port =  process.env.PORT
 
 const app = express()
@@ -9,6 +11,8 @@ const User = require("./models/User")
 const Task = require("./models/Task")
 
 app.use(express.json())
+
+app.use("/user",UserRoutes)
 
 connection.sync().then(
     app.listen(port)
