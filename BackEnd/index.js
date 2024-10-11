89e7ba74f870cdc5f4a1ea226d40ce4188784a3e
MarcosMartinsDeLima <0timetraveler@gmail.com>
2024-10-11 14:21:10 -0300

    fix: erro de cors

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const connection = require("./db/connection")
 const redisClient = require("./db/redisConnection")
+const cors = require('cors');
 
 const UserRoutes = require("./routes/UserRoutes")
 const TaskRoutes = require("./routes/TaskRoutes")
@@ -13,6 +14,24 @@ const User = require("./models/User")
 const Task = require("./models/Task")
 
 app.use(express.json())
+
+app.use((req, res, next) => {
+
+    // Qualquer endereço pode fazer requisição "*"
+    res.header("Access-Control-Allow-Origin", "*");
+
+    // Tipos de método que a API aceita
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+
+    // Permitir o envio de dados para API
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+
+    // Executar o cors
+    app.use(cors());
+
+    // Quando não houver erro deve continuar o processamento
+    next();
+});
 
 app.use("/user",UserRoutes)
 app.use("/task",TaskRoutes)
